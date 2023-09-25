47dd00a2acb2c9a83654d45192ca474a74bc57e3
SergeyChudinov <SergeyChudinov1986@gmail.com>
2023-09-25 14:49:00 +0500

    Make 1.4.14

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,27 +1,56 @@
+import React, { Component } from 'react';
 import "./todo-list-item.css";
 
-function TodoListItem({ description, created, onToggleProp, onDeleted }) {
-  return (
-    <>
-      <div className="view">
-        <input className="toggle" type="checkbox" />
-        <label>
-          <span
-            className="description"
-            onClick={() => onToggleProp("class")}
-          >
-            {description}
-          </span>
-          <span className="created">{created}</span>
-        </label>
-        <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy"
-          onClick={onDeleted}
-        ></button>
-      </div>
-      <input type="text" className="edit" defaultValue="Editing task" />
-    </>
-  );
+class TodoListItem extends Component {
+  state = {
+    label: this.props.description,
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemChange(this.state.label);
+  };
+
+  render() {
+    const { description, created, onToggleProp, onDeleted, completed } =
+      this.props;
+
+    return (
+      <>
+        <div className="view">
+          <input className="toggle" type="checkbox" checked={completed} />
+          <label>
+            <span
+              className="description"
+              onClick={() => onToggleProp("completed")}
+            >
+              {description}
+            </span>
+            <span className="created">{created}</span>
+          </label>
+          <button
+            className="icon icon-edit"
+            onClick={() => onToggleProp("editing")}
+          ></button>
+          <button className="icon icon-destroy" onClick={onDeleted}></button>
+        </div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            className="edit"
+            onChange={this.onLabelChange}
+            value={this.state.label}
+          />
+        </form>
+      </>
+    );
+  }
 }
 
 export default TodoListItem;
--- todo/src/components/todo-list/todo-list.js
+++ todo/src/components/todo-list/todo-list.js
@@ -1,15 +1,22 @@
 import TodoListItem from '../todo-list-item';
 import "./todo-list.css";
 
-function TodoList({ todos, onToggleProp, onDeleted }) {
-	
+function TodoList({ todos, onToggleProp, onDeleted, onItemChange }) {
 	const elements = todos.map(({ id, ...props }) => {
+		let classNames = "";
+		if (props.editing) {
+      classNames += " editing";
+    } 
+		if (props.completed) {
+      classNames += " completed";
+    }
 		return (
-      <li key={id} className={props.class}>
+      <li key={id} className={classNames}>
         <TodoListItem
           {...props}
           onToggleProp={(prop) => onToggleProp(id, prop)}
           onDeleted={() => onDeleted(id)}
+          onItemChange={(text) => onItemChange(id, text)}
         />
       </li>
     );
